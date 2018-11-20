1d6e6f7796a2a288c7deef0fed10bb04b0b7a788
Elliot Gagola <egagola@spartaglobal.com>
2018-11-20 17:34:34 +0000

    V.1.0 Re-re-refactored and added class names

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,24 +1,30 @@
-#Full one
-def fizzBuzz maxNum
-  a = Array.new
-  for i in 1 .. maxNum
-    if i % 15 == 0
-      num = "FizzBuzz"
-      a.push(num)
-    elsif i % 5 == 0
-      num = "Buzz"
-      a.push(num)
-    elsif i % 3 == 0
-      num = "Fizz"
-      a.push(num)
-    else
-      a.push(i)
-    end
-  end
-  a
-end
+class FizzBuzz
 
-#Number division
-def div num1, divNum
-  num1 % divNum == 0
+  def initialize (start, finish)
+    @start, @finish = start, finish
+  end
+
+  def fizzBuzz
+    a = Array.new
+    for i in @start .. @finish
+      if div(i,15) == true
+        num = "FizzBuzz"
+        a.push(num)
+      elsif div(i,5) == true
+        num = "Buzz"
+        a.push(num)
+      elsif div(i,3) == true
+        num = "Fizz"
+        a.push(num)
+      else
+        a.push(i)
+      end
+    end
+    a
+  end
+
+  #Number division
+  def div num1, divNum
+    num1 % divNum == 0
+  end
 end
