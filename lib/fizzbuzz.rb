9f4c353713103f93b720470e2a39c069449a8d9f
Elliot Gagola <egagola@spartaglobal.com>
2018-11-20 17:18:44 +0000

    V.0.3 Refactored

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -18,58 +18,15 @@ def fizzBuzz maxNum
   a
 end
 
-# Only applicable for multiples of fifteen
-def pushFifteen maxNum
-  a = Array.new
-  for i in 1 .. maxNum
-    if i % 15 == 0
-      num = "FizzBuzz"
-      a.push(num)
-    else
-      a.push(i)
-    end
-  end
-  a.last
-end
-
-# Applicable for multiples of five
-def pushFive maxNum
-  a = Array.new
-  for i in 1 .. maxNum
-    if i % 5 == 0
-      num = "Buzz"
-      a.push(num)
-    else
-      a.push(i)
-    end
-  end
-  a.last
-end
-
-# Applicable for multiples of 3
-def pushThree maxNum
-  a = Array.new
-  for i in 1 .. maxNum
-    if i % 3 == 0
-      num = "Fizz"
-      a.push(num)
-    else
-      a.push(i)
-    end
-  end
-  a.last
-end
-
 #Number division
-def div num1
-  if num1 % 15 == 0
-    a = true
-  elsif num1 % 5 == 0
-    a = true
-  elsif num1 % 3 == 0
-    a = true
-  else
-    a = false
-  end
-  a
+def div3 num1
+  num1 % 3 == 0
+end
+
+def div5 num1
+  num1 % 5 == 0
+end
+
+def div15 num1
+  num1 % 15 == 0
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,26 +2,12 @@ require 'spec_helper'
 
 describe 'Method' do
   it "Should know when a number is divisible" do
-    expect(div(15)).to eq(true)
-    expect(div(12)).to eq(true)
-    expect(div(5)).to eq(true)
-    expect(div(22)).to eq(false)
-  end
-
-  it "Should know when a multiple of three is entered" do
-    expect(pushThree(3)).to eq("Fizz")
-    expect(pushThree(7)).to eq(7)
-  end
-
-  it "Should know when a multiple of five is entered" do
-    expect(pushFive(5)).to eq("Buzz")
-    expect(pushFive(7)).to eq(7)
-  end
-
-  it "Should know when a multiple of fifteen is entered" do
-    expect(pushFifteen(15)).to eq("FizzBuzz")
-    expect(pushFifteen(7)).to eq(7)
-
+    expect(div15(15)).to eq(true)
+    expect(div3(12)).to eq(true)
+    expect(div5(5)).to eq(true)
+    expect(div15(22)).to eq(false)
+    expect(div5(22)).to eq(false)
+    expect(div3(22)).to eq(false)
   end
 
   it "Should work for all values" do
